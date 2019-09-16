a001cc563fb883abdf8c10453f2869f9d0234a27
arkazero <jugarcia@gmail.com>
2019-09-16 15:01:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is debe sacar error por punto y coma" . PHP_VERSION ."- Error matematico:". $a=10/10. " Valor de a:". $a."\n"
+print "Hello, World! php version is debe sacar error por punto y coma" . PHP_VERSION ."- Error matematico:". $a=10/10."\n"
+echo $a;  
 ?>
