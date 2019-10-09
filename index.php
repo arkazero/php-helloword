c04b7183bf4abbbbfffbca9896ec4125221afb10
arkazero <jugarcia@gmail.com>
2019-10-09 10:13:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 print "Hello, World! php version is " . PHP_VERSION . "Error matematico: \n";
 $a=10/10
 echo $a;  
+echo "<br />";
+echo "El valor de la variable que llega desde el config map es: ".$_ENV["MSG"]; 
 ?>
