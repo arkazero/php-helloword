97aadc1d6183f9fb8d09c1987f413212905f57e2
arkazero <jugarcia@gmail.com>
2020-03-10 18:46:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo $a;
 echo "<br />";
 echo "El valor de la variable que llega desde el config map es: ".$_ENV["MENSAJE"]["MSG"] . "--- \n ". $_ENV["MENSAJE"] . " ----\n ". $_ENV["SUMMARY"] ; 
 
-print_r ($_ENV);
+
 ?>
