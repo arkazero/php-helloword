a0345abc0e63da471d9d8e0a2e7a8aab0695c508
arkazero <jugarcia@gmail.com>
2022-05-26 07:04:21 -0500

    Update index.php
    Adiciones para comprobar funcionamiento de los mensajes de error

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
+error_log("Envío de mensajes al log --  Iniciando ejercicios");
 print "Hello, World! php version is " . PHP_VERSION . "Ya no hay error matematico: \n";
 $a=10/10;
+error_log("Envío de mensajes al log --  El valor de la operacion es: ".$a);
 echo $a;  
 echo "<br />";
 echo "El valor de la variable que llega desde el config map es: ".$_ENV["MENSAJE"]["MSG"] . "--- \n ". $_ENV["MENSAJE"] . " ----\n ". $_ENV["SUMMARY"] ; 
-
+error_log("Envío de mensajes al log --  Finaliza el ejercicio ");
 
 ?>
